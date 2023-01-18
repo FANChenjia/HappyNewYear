cdb97ce3317fd93cbebe672ef85ab28d4569f606
MingS <chenjia.fan@icloud.com>
2023-01-18 22:35:45 +0800

    Updated

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,5 @@
+//函数定义区域
+//Cookie基本函数
 function setCookie(cname, cvalue, exdays) {
     var d = new Date();
     d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
@@ -13,19 +15,39 @@ function getCookie(cname) {
     }
     return "";
 }
+//Colokie基本函数===
 
 // 函数定义区域===
 
+//代码区域
 
-//代码区域===
-
-rNumber = Math.floor(Math.random() * 10);
-if (getCookie("oNumber") == 1) {
-    alert(getCookie("rNumber"))
+//rNumber:0-9随机数
+//根据rNumber不同显示不同页面
+//oNumber用户打开页面次数
+var rSites = '{ "sites":[' +
+'{"title": "上上签", "text": "时来运转,和乐平安"},' +
+'{"title": "上上签", "text": "喜气盈门,岁且更始"},' +
+'{"title": "上吉签", "text": "岁岁平安,年年有余"},' +
+'{"title": "上吉签", "text": "四时如意,万事遂心"},' +
+'{"title": "大吉签", "text": "吉庆有余,福享新春"},' +
+'{"title": "大吉签", "text": "心想事成,五福临门"},' +
+'{"title": "中吉签", "text": "满院春光,庆云跃日"},' +
+'{"title": "中吉签", "text": "门凝瑞霭,户发春光"},' +
+'{"title": "中吉签", "text": "时乃日新,三阳开泰"},' +
+'{"title": "中吉签", "text": "金玉满堂,喜气洋洋"} ]}';
+//oNumber=1时
+if (getCookie("oNumber") == 1) { //已经打开过了
+    obj = JSON.parse(rSites);
+    var cookieRnumber = getCookie("rNumber") + 1;
+    document.getElementById("rTitle").innerHTML = obj.sites[cookieRnumber].title;
+    document.getElementById("rMain").innerHTML = obj.sites[cookieRnumber].text;
 }
-else {
-    alert(rNumber) //操作
-    setCookie("oNumber",1,0) //实际使用将0改成7
-    setCookie("rNumber",rNumber,0) //实际使用将0改成7
-
+else { //未打开过
+    var rNumber = Math.floor(Math.random() * 10);
+    obj = JSON.parse(rSites);
+    setCookie("oNumber", 1, 7); //实际使用将0改成7
+    setCookie("rNumber", rNumber, 7); //实际使用将0改成7
+    var cookieRnumber = getCookie("rNumber") + 1;
+    document.getElementById("rTitle").innerHTML = obj.sites[cookieRnumber].title;
+    document.getElementById("rMain").innerHTML = obj.sites[cookieRnumber].text;
 }
