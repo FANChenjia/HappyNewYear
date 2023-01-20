aef4ecfc1f33bd6a4628f3a1f400d113688359ac
MingS <chenjia.fan@icloud.com>
2023-01-20 15:37:39 +0800

    RC Update

--- assets/script.js
+++ assets/script.js
@@ -26,16 +26,16 @@ function getCookie(cname) {
 //oNumber用户打开页面次数
 var rNumber = Math.floor(Math.random() * 10);
 var rSites = '{ "sites":[' +
-'{"title": "上上签", "text": "时来运转,和乐平安"},' +
-'{"title": "上上签", "text": "喜气盈门,岁且更始"},' +
-'{"title": "上吉签", "text": "岁岁平安,年年有余"},' +
-'{"title": "上吉签", "text": "四时如意,万事遂心"},' +
-'{"title": "大吉签", "text": "吉庆有余,福享新春"},' +
-'{"title": "大吉签", "text": "心想事成,五福临门"},' +
-'{"title": "中吉签", "text": "满院春光,庆云跃日"},' +
-'{"title": "中吉签", "text": "门凝瑞霭,户发春光"},' +
-'{"title": "中吉签", "text": "时乃日新,三阳开泰"},' +
-'{"title": "中吉签", "text": "金玉满堂,喜气洋洋"} ]}';
+'{"title": "上上签", "text": "时来运转 和乐平安"},' +
+'{"title": "上上签", "text": "喜气盈门 岁且更始"},' +
+'{"title": "上吉签", "text": "岁岁平安 年年有余"},' +
+'{"title": "上吉签", "text": "四时如意 万事遂心"},' +
+'{"title": "大吉签", "text": "吉庆有余 福享新春"},' +
+'{"title": "大吉签", "text": "心想事成 五福临门"},' +
+'{"title": "中吉签", "text": "满院春光 庆云跃日"},' +
+'{"title": "中吉签", "text": "门凝瑞霭 户发春光"},' +
+'{"title": "中吉签", "text": "时乃日新 三阳开泰"},' +
+'{"title": "中吉签", "text": "金玉满堂 喜气洋洋"} ]}';
 var obj = JSON.parse(rSites);
 //oNumber=1时
 if (getCookie("coNumber") == 1) { //已经打开过了
